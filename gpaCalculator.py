a70344978a65e80e4265c65e671903101b0099a3
safwanx <107620033+safwanx@users.noreply.github.com>
2023-08-14 23:50:42 +0300

    Update gpaCalculator.py

--- gpaCalculator.py
+++ gpaCalculator.py
@@ -1,37 +1,43 @@
-previousGPA = float(input("Enter current GPA: "))
-previousCredits = int(input("Enter previously earned credits: "))
+def get_input(prompt, input_type):
+    while True:
+        try:
+            user_input = input(prompt)
+            converted_input = input_type(user_input)
+            return converted_input
+        except ValueError:
+            print("Invalid input. Please enter the correct input type.")
+
+previousGPA = get_input("Enter current GPA: ", float)
+previousCredits = get_input("Enter previously earned credits: ", int)
 creditPoints5 = float(previousGPA * previousCredits)
 
-grade = ['A+','A' ,'B+','B','C+','C','D+','D', 'F']
+grade = ['A+', 'A', 'B+', 'B', 'C+', 'C', 'D+', 'D', 'F']
 gradePoints5 = [5, 4.75, 4.5, 4, 3.5, 3, 2.5, 2, 0]
-classesNumbers = int(input("Enter no. of classes being taken: "))
+classesNumbers = get_input("Enter no. of classes being taken: ", int)
 classes = []
 grades = []
 credits = []
-#input the classes, grades, and credits
+
+# input the classes, grades, and credits
 for i in range(classesNumbers):
     classes.append(input("Enter class name: "))
-    grades.append(input("Enter grade: ").capitalize())
-    credits.append(int(input("Enter credits: ")))
+    grades.append(get_input("Enter grade: ", str).capitalize())
+    credits.append(get_input("Enter credits: ", int))
 
-
-#calculating credit points for gpa 
+# calculating credit points for gpa
 creditPoints = 0
 for i in range(len(grades)):
     creditPoints += gradePoints5[grade.index(grades[i])] * credits[i]
 
-
-#calculate the credit points for cgpa
+# calculate the credit points for cgpa
 for i in range(len(grades)):
     creditPoints5 += gradePoints5[grade.index(grades[i])] * credits[i]
 
-
-#calculate the gpa and cgpa for 5.0 scale
+# calculate the gpa and cgpa for 5.0 scale
 gpa = round(creditPoints / sum(credits), 3)
 cgpa = round(creditPoints5 / (previousCredits + sum(credits)), 3)
 
-
-#printing all info in table format and remove extra spaces to adjust the table
+# printing all info in table format and remove extra spaces to adjust the table
 print("\n")
 print("Class".ljust(20) + "Grade".ljust(20) + "Credits".ljust(20))
 for i in range(len(grades)):
